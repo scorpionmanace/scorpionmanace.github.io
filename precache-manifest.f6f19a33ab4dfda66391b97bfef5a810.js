f0da0be114cfe91dc1cd12cf9a677d8c87104b4b
scorpionmanace <karan.spartan90@gmail.com>
2019-09-05 23:28:59 -0700

    deploy

---
+++ precache-manifest.f6f19a33ab4dfda66391b97bfef5a810.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "27d53680610e0848d43a151495337b22",
+    "url": "/resume.pdf"
+  },
+  {
+    "revision": "e0ae839cd50619eb8278",
+    "url": "/js/chunk-vendors.c27abd63.js"
+  },
+  {
+    "revision": "43333510adfd82d0b5a1",
+    "url": "/js/app.eeb38d39.js"
+  },
+  {
+    "revision": "9988f9f891604f50be7b",
+    "url": "/js/about.fbc27f2f.js"
+  },
+  {
+    "revision": "e9659c6be47c08e2212fbc9e360c1949",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "e0ae839cd50619eb8278",
+    "url": "/css/chunk-vendors.3aea81fb.css"
+  }
+];
